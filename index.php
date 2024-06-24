516b6567f8e3d675c49b716bfa8cf95cc8989776
miguelsantossyone <123757592+miguelsantossyone@users.noreply.github.com>
2024-06-24 11:41:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Hello World!";
+// Function to generate a random color in hexadecimal format
+function random_color() {
+    return '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
+}
+
+// Generate a random color
+$color = random_color();
+
+// Output the HTML with inline CSS to change the color dynamically
+echo '<div style="color: ' . $color . '">';
+echo 'Hello ' .$_ENV["USER"] . '!';
+echo '</div>';
+
+?>
